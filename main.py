b14f52f2d7691f16f96a701434abe444d7fc2f69
Thomas Culino <thomas.culino@gmail.com>
2025-06-24 09:51:08 -0700

    name fix

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import time
 from baselayer.log import make_log
 
-log = make_log("example_external_service")
+log = make_log("external_service_example_doc")
 
 def main():    
     start_time = time.time()
     while True:
       elapsed_time = time.time() - start_time
-      log(f"Running example_external_service for {elapsed_time} seconds")
+      log(f"Running external_service_example_doc for {elapsed_time} seconds")
       time.sleep(2)
 
 if __name__ == "__main__":
